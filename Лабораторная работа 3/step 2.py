6f1868aed1a71ea32d6a2a832567f3c02e306870
wendell-weber <147609879+wendell-weber@users.noreply.github.com>
2023-10-30 14:10:34 +0300

    Add files via upload

--- Лабораторная работа 3/step 2.py
+++ Лабораторная работа 3/step 2.py
@@ -12,5 +12,5 @@ participants_first_group = "Иванов|Петров|Сидоров"
 participants_second_group = "Петров|Сидоров|Смирнов"
 
 # TODO Провеьте работу функции с разделителем отличным от запятой
-participants = find_common_participants(participants_first_group, participants_second_group)
+participants = find_common_participants(participants_first_group, participants_second_group, separator='|')
 print(participants)
